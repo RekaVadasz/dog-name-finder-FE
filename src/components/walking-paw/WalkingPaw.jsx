0ab897422a7dfdfa316c0c1c13ef087a573a7b9b
RekaVadasz <vadasz.reka87@gmail.com>
2022-10-26 20:48:51 +0200

    Complete WalkingPaw animation at Home page

--- src/components/walking-paw/WalkingPaw.jsx
+++ src/components/walking-paw/WalkingPaw.jsx
@@ -2,31 +2,37 @@ import React from 'react';
 import paw from '../../assets/paw-horizontal.svg';
 import './WalkingPaw.css';
 
-
-
 export default function WalkingPaw() {
-  return (
-
-    <div className='paw-animation-container'>
-        <div className="paw-container">
-            <img className="paw-icon" src={paw} alt='paw'></img>
+    return (
+        <div className='paw-animation-container'>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
+            <div className="paw-container"><img className="paw-icon" src={paw} alt='paw'></img></div>
         </div>
-        <div className="paw-container">
-            <img className="paw-icon" src={paw} alt='paw'></img>
-        </div>
-        <div className="paw-container">
-            <img className="paw-icon" src={paw} alt='paw'></img>
-        </div>
-        <div className="paw-container">
-            <img className="paw-icon" src={paw} alt='paw'></img>
-        </div>
-        <div className="paw-container">
-            <img className="paw-icon" src={paw} alt='paw'></img>
-        </div>
-        <div className="paw-container">
-            <img className="paw-icon" src={paw} alt='paw'></img>
-        </div>
-    </div>
-
-  )
+    )
 }
